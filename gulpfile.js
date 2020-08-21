e3defe077d997929c735813b6544d6fe2683ea50
moeclay <moeamarreza@gmail.com>
2020-08-21 14:16:27 +0700

    push with watch and list

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulpMinifyCss = require('gulp-minify-css');
 var gulpConcat = require('gulp-concat');
 var gulpUglify = require('gulp-uglify');
 var gulpHtmlmin = require('gulp-htmlmin');
+var gulpConnect = require('gulp-connect');
+var clean = require('gulp-clean');
 
 // css
 gulp.task('minify-css', async () => {
@@ -10,7 +12,8 @@ gulp.task('minify-css', async () => {
     .pipe(gulpMinifyCss({
       compatibility: 'ie8'
     }))
-    .pipe(gulp.dest('./dist/css/'));
+    .pipe(gulp.dest('./dist/'))
+    .pipe(gulpConnect.reload());
   console.log('success minify-css');
 });
 
@@ -23,7 +26,8 @@ gulp.task('minify-js', async () => {
     ])
     .pipe(gulpConcat('bundle.js'))
     .pipe(gulpUglify())
-    .pipe(gulp.dest('dist/js/'));
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(gulpConnect.reload());
   console.log('success minify-js');
 });
 
@@ -33,6 +37,35 @@ gulp.task('minify-html', async () => {
     .pipe(gulpHtmlmin({
       collapseWhitespace: true
     }))
-    .pipe(gulp.dest('dist/html/'));
-  console.log('success minify-html');
+    .pipe(gulp.dest('dist/html/'))
+    .pipe(gulpConnect.reload());
+    console.log('success minify-html');
 });
+
+
+// server
+gulp.task('server', async function(){
+  gulpConnect.server({
+    root: "dist",
+    livereload: true
+  });
+});
+
+// clean build
+gulp.task('clean', function(){
+  return gulp.src('dist', {
+    read: false,
+    allowEmpty: true
+  }).pipe(clean());
+});
+
+// perubahan file aplication
+gulp.task('watch', async function(){
+  gulp.watch('./src/js/*.js', gulp.series('minify-js'));
+  gulp.watch('./src/css/*.css', gulp.series('minify-css'));
+  gulp.watch('./src/html/*.html', gulp.series('minify-html'));
+});
+
+// default
+gulp.task('default', gulp.series('watch','server'));
+gulp.task('build', gulp.series('clean', 'minify-css', 'minify-js', 'minify-html'))
